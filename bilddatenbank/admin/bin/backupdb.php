89e4fcf66bb7115f367fbc6c507fe503960f8fc1
pwafzig <pe.wafzig@gmail.com>
2023-09-28 21:01:26 +0200

    Bump SQL Backups to PHP8

--- bilddatenbank/admin/bin/backupdb.php
+++ bilddatenbank/admin/bin/backupdb.php
@@ -1,35 +1,10 @@
-<?php include("../../php/includes/start.inc.php"); ?>
-<?php
+<?php 
+	include("../../php/includes/start.inc.php");
 
-	$f = fopen(DOCROOT.INSTALLPATH."/admin/backup/".date('YmdHis')."_backup.sql", "w");
+	$backupfile = $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH."/admin/backup/".date('YmdHis')."_backup.sql";
 
-	$tables = mysql_list_tables(DB_NAME);
-
-	while ($cells = mysql_fetch_array($tables)) {
-
-		$table = $cells[0];
-		fwrite($f,"DROP TABLE `$table`;\n");
-		$res = mysql_query("SHOW CREATE TABLE `$table`");
-		if ($res) {
-		$create = mysql_fetch_array($res);
-		$create[1] .= ";";
-		$line = str_replace("\n", "", $create[1]);
-		fwrite($f, $line."\n");
-		$data = mysql_query("SELECT * FROM `$table`");
-		$num = mysql_num_fields($data);
-		while ($row = mysql_fetch_array($data)){
-		$line = "INSERT INTO `$table` VALUES(";
-		for ($i=1;$i<=$num;$i++) {
-		$line .= "'".mysql_real_escape_string($row[$i-1])."', ";
-		}
-		$line = substr($line,0,-2);
-		fwrite($f, $line.");\n");
-		}
-		}
-	}
-
-	fclose($f);
-
+	exec("mysqldump --user=".DB_USER." --password=".DB_PWD." --host=".DB_HOST." ".DB_NAME." > ".$backupfile."");
+	
 	header("Location:http://".$_SERVER['HTTP_HOST']."/".INSTALLPATH."/admin/index.php?sect=backup");
 
 ?>
