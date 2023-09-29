656f2ed4b0d5952eb004fe59aaa916073f982ba4
pwafzig <pe.wafzig@gmail.com>
2023-09-29 13:52:37 +0200

    Fix mailing in register and mailer

--- bilddatenbank/mailer.php
+++ bilddatenbank/mailer.php
@@ -1,124 +1,110 @@
 <?php include("php/includes/start.inc.php"); ?>
 <?php
 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	use PHPMailer\PHPMailer\SMTP;
+
 	//Ueberpruefe, ob eine Session vorhanden ist
     if(isset($_SESSION['login'])) {
 
 		//Ueberpruefe, ob eine id uebergeben wurde
 		if(isset($_GET['id'])){
-
 				$id = intval(@$_GET['id']);
-				$id = mysql_real_escape_string($_GET['id']);
-
 		} else {
-
 			//Wenn keine id uebergeben wurde
 			echo "ID failed...";
-			exit;
-
-		}
-
-		//Abfrage ob HighRes- oder LowRes-User
-		if(isset($_SESSION['resolution'])){
-			if($_SESSION['resolution'] == "highres"){
-				$data = "data";
-			} else {
-				$data = "lowres";
-			} 
-		} else {
 			exit;
 		}
 		
 		//Datenbankabfrage
 		$stmt_popup = "SELECT * FROM picture_data WHERE id = '$id'";
-		$query_popup = mysql_query($stmt_popup);
-		$result_popup = mysql_fetch_array($query_popup);
-
+		$query_popup = mysqli_query($link, $stmt_popup);
+		$result_popup = mysqli_fetch_array($query_popup, MYSQLI_ASSOC);
+	
 		//Ueberpruefe, ob die id in der Datenbank existiert
 		if(isset($result_popup['id'])){
-
+	
 			//Sicherheitshalber den filename nochmal bereinigen
 			$filename = ltrim(rtrim($result_popup['filename']));
-
+	
 			//Logging des Downloads
 			if($_SESSION['login'] != "probezugang"){
 				$stmt_log = "UPDATE users SET downloads = downloads+1 WHERE login = '".$_SESSION['login']."' LIMIT 1";
-	            mysql_query($stmt_log);
+	            mysqli_query($link, $stmt_log);
 	            $stmt_count = "INSERT INTO downloads VALUES (NULL, '".$_SESSION['login']."', '".$result_popup['filename']."', CURRENT_TIMESTAMP, '".$_SERVER['REMOTE_ADDR']."')";
-	            mysql_query($stmt_count);
+	            mysqli_query($link, $stmt_count);
 	        } else {
 	            $stmt_log = "UPDATE accessids SET downloads = downloads+1 WHERE hash = '".$_SESSION['accessid']."' LIMIT 1";
-	            mysql_query($stmt_log);
+	            mysqli_query($link, $stmt_log);
 	            $stmt_count = "INSERT INTO downloads VALUES (NULL, '".$_SESSION['name']."', '".$result_popup['filename']."', CURRENT_TIMESTAMP, '".$_SERVER['REMOTE_ADDR']."')";
-	            mysql_query($stmt_count);
+	            mysqli_query($link, $stmt_count);
 	        }
 
-			require_once(DOCROOT.INSTALLPATH."/lib/PHPMailer/class.phpmailer.php");
+			require $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/src/Exception.php';
+			require $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/src/PHPMailer.php';
+			require $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/src/SMTP.php';
 
-			$mail = new PHPMailer();
-
-			$body = nl2br($CONFIG['mailer_text']);
-
-			$mail->CharSet = "utf8";
-			$mail->AddReplyTo($CONFIG['email'],$CONFIG['name']);
-			$mail->SetFrom($CONFIG['sendemail'], $CONFIG['name']);
-
-			$address = $_SESSION['email'];
-			$mail->AddAddress($address, $_SESSION['name']);
-
-			$mail->Subject    = "Bildzusendung Bild Nr. ".$result_popup['id']."";
-			$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-
-			$mail->MsgHTML($body);
-
-			$mail->AddAttachment(DOCROOT.INSTALLPATH."/".$data."/".$filename);
-
-			if(!$mail->Send()) {
-				echo "Tut uns leid, die Email konnte nicht zugestellt werden. Bitte wenden Sie sich an den Administrator: " . $mail->ErrorInfo;
+			//Auflösung für die ZIP-Datei festlegen
+			if($_SESSION['resolution'] == "lowres") { 
+				$res_path = "lowres"; 
 			} else {
+				$res_path = "data";
+			}
 
-				//Infomail senden
-				$file	 = "http://".$_SERVER['HTTP_HOST']."/".INSTALLPATH."/thumbs/".$filename;
-				$to      = $CONFIG['email'];
-				$subject = "Download Bilddatenbank";
-				$headers = "From: ".$CONFIG['name']." <".$CONFIG['sendemail'].">" . "\r\n" .
-		    				"Reply-To: ".$CONFIG['sendemail']."" . "\r\n" .
-		    				"X-Mailer: PHP/" . phpversion();
+			//Create a new PHPMailer instance
+			$mail = new PHPMailer;
+			$mail->isSMTP(); 
 
-		    	$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+			$mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+			$mail->Host = $CONFIG['smtphost'];
+			$mail->Port = $CONFIG['smtpport']; 
+			$mail->SMTPSecure = 'tls'; // ssl is depracated
+			$mail->SMTPAuth = true;
+			$mail->Username = $CONFIG['smtplogin'];
+			$mail->Password = $CONFIG['smtppass'];
+		    
+		    //Recipients
+		    $mail->setFrom($CONFIG['sendemail'], $CONFIG['firma']);
+		    $mail->addAddress($_SESSION['email'], $_SESSION['name']);
+		    $mail->addReplyTo($CONFIG['sendemail'], $CONFIG['firma']);
+		    
+		    //Attachments
+		    $mail->addAttachment($_SERVER['DOCUMENT_ROOT'].'/bilddatenbank/'.$res_path.'/'.$filename);
 
-				$message  = "Art: Mailversand\r\n";
-				$message .= "Remote-IP: " . $_SERVER['REMOTE_ADDR'] . "\r\n";
-				$message .= "Remote-Host: " . $hostname . "\r\n";
-				$message .= "Remote-Browser: " . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
-				$message .= "Uhrzeit: " . date("d.m.Y, H:i:s") . "\r\n";
-				$message .= "Datei: " . $file . "\r\n";
-				$message .= "Aufloesung: " . $_SESSION['resolution'] . "\r\n";
+		    //Content
+		    $mail->Subject = 'Bildzusendung Bild Nr. '.$result_popup['id'].'';
+		    $mail->msgHTML(file_get_contents($_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/contents.html'), __DIR__);
+			$mail->AltBody = 'Bildzusendung Bild Nr. '.$result_popup['id'].'';
 
-				mail($to, $subject, $message, $headers, "-f ".$CONFIG['sendemail']."");
-
-				//echo "Message sent!";
-				if(ereg("detail",$_SERVER['HTTP_REFERER'])){
-					header("Location:".$_SERVER['HTTP_REFERER']."&mail=sent");
+		    if (!$mail->send()) {
+			    echo 'Mailer Error: ' . $mail->ErrorInfo;
+			} else {
+			    //echo 'Message sent!';
+			}
+				
+			//Infomail senden
+		
+			if(isset($_SERVER['HTTP_REFERER'])) {
+				if(preg_match("/detail/",$_SERVER['HTTP_REFERER'])){
+					header("Location:".$_SERVER['HTTP_REFERER']."?mail_ok=yes");
 				} else {
 					header("Location:".$_SERVER['HTTP_REFERER']);
-				}
+				} 				
+			} else {
+				header("Location:".$_SERVER['HTTP_REFERER']."?mail_ok=yes");
 			}
 
 		} else {
-
 			//Wenn id nicht in der Datenbank vorhanden
 			echo "ID failed...";
 			exit;
-
 		}
-
+	
 	} else {
-
 		//Wenn keine Session vorhanden war
 		echo "Nice try...";
 		exit;
-
 	}
 
 ?>
--- bilddatenbank/register.php
+++ bilddatenbank/register.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 	<title>Registrierung</title>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" language="javascript"></script>
-	<script type="text/javascript" src="/".<?php echo INSTALLPATH; ?>."/lib/jquery-validate/jquery.validate.pack.js" language="javascript"></script>
+	<script type="text/javascript" src="/bilddatenbank/lib/jquery/jquery.min.js" language="javascript"></script>
+	<script type="text/javascript" src="/bilddatenbank/lib/jquery-validate/jquery.validate.pack.js" language="javascript"></script>
 	<style>
 		body {
 			font-family: Verdana, Arial, Sans-Serif;
 			font-size: 9pt;
 		}
-
+		
 		label {
 			width:120px;
 			margin: 3px 0px 10px 0px;
@@ -18,18 +18,18 @@
 			font-family: Verdana, Arial, Sans-Serif;
 			font-size: 9pt;
 		}
-
+		
 		label.error {
 			width: 250px;
 			float: right;
 			color: #FF0000;
 			font-size: 8pt;
 		}
-
+		
 		p {
 			clear:both;
 		}
-
+		
 		input {
 			margin: 0px 0px 10px 0px;
 			font-family: Helvetica, Arial, Sans-Serif;
@@ -38,7 +38,7 @@
 			width: 250px;
 
 		}
-
+		
 		.button {
 			color: #FFFFFF;
 			background-color:#000;
@@ -91,56 +91,67 @@
 <body>
 <div style="width:770px">
 
-<?php
+<?php 
+	
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+	use PHPMailer\PHPMailer\SMTP;
 
-	if(isset($_POST['send']) && $_POST['send'] == "true"){
+	if(!empty($_POST['send']) && $_POST['send'] == "true"){
 
-		require_once(DOCROOT.INSTALLPATH."/lib/PHPMailer/class.phpmailer.php");
+		require $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/src/Exception.php';
+		require $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/src/PHPMailer.php';
+		require $_SERVER['DOCUMENT_ROOT']."/".INSTALLPATH.'/lib/PHPMailer/src/SMTP.php';
 
-		$mail = new PHPMailer();
+		//Create a new PHPMailer instance
+		$mail = new PHPMailer;
+		$mail->isSMTP(); 
 
-		$mailout  = "Registrierungsanfrage von ".$_POST['name']."\n";
-		$mailout .= "Email-Adresse ".$_POST['email']."\n";
-		$mailout .= "Redaktion ".$_POST['redaktion']."\n";
-		$mailout .= "Telefon ".$_POST['telefon']."\n";
-		$mailout .= "\n\n----------------------------------\n";
+		$mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+		$mail->Host = $CONFIG['smtphost'];
+		$mail->Port = $CONFIG['smtpport']; 
+		$mail->SMTPSecure = 'tls'; // ssl is depracated
+		$mail->SMTPAuth = true;
+		$mail->Username = $CONFIG['smtplogin'];
+		$mail->Password = $CONFIG['smtppass'];
+	    
+	    //Recipients
+	    $mail->setFrom($CONFIG['sendemail'], $CONFIG['firma']);
+	    $mail->addAddress($CONFIG['sendemail'], $CONFIG['firma']);
+	    $mail->addReplyTo($_POST['email'], $_POST['name']);
 
-		$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+	    //Body
+	    $mailout  = "Registrierungsanfrage von: ".$_POST['name']."<br />";
+		$mailout .= "Email-Adresse: ".$_POST['email']."<br />";
+		$mailout .= "Redaktion: ".$_POST['redaktion']."<br />";
+		$mailout .= "Telefon: ".$_POST['telefon']."<br />";
+		$mailout .= "<br /><br />----------------------------------<br />";
 
-		$mailout .= "Remote-IP: " . $_SERVER['REMOTE_ADDR'] . "\r\n";
-		$mailout .= "Remote-Host: " . $hostname . "\r\n";
-		$mailout .= "Remote-Browser: " . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
-		$mailout .= "Uhrzeit: " . date("d.m.Y, H:i:s") . "\r\n";
-
-		$body = $mailout;
-
-		$mail->AddReplyTo($CONFIG['email'],$CONFIG['name']);
-		$mail->SetFrom($CONFIG['sendemail'], $CONFIG['name']);
-		$mail->AddAddress($CONFIG['email'], $CONFIG['name']);
-		$mail->Subject = "Registrierungsanfrage";
-		$mail->Body = $mailout;
-
-		if(!$mail->Send()) {
+	    //Content
+	    $mail->Subject = 'Registrierungsanfrage';
+	    $mail->msgHTML($mailout);
+		
+		if(!$mail->send()) {
 			echo "Tut uns leid, die Email konnte nicht zugestellt werden. Bitte wenden Sie sich an den Administrator: " . $mail->ErrorInfo;
 		} else {
 			echo "Ihre Anfrage wurde gesendet, herzlichen Dank! <button onclick=\"self.parent.tb_remove();\" class=\"button\">Schlie&szlig;en</button>";
 		}
-
+			
 	} else {
 ?>
 
 	<h3 style="margin:20px 0 0 50px;">Registrierung beantragen:</h3>
 	<p style="margin:10px 50px 0 50px;">Um die volle Funktionalit&auml;t der Datenbank nutzen zu k&ouml;nnen, m&uuml;ssen Sie sich registrieren lassen. Eine Registrierung ist nur f&uuml;r gewerbliche Kunden m&ouml;glich.</p>
-
+	
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" id="register" name="register">
-	<fieldset name="register" style="margin:20px 50px 50px 50px;border:none;background-color:#ebebeb;padding:20px">
+	<fieldset name="register" style="margin:20px 50px 50px 50px;border:none;background-color:#ebebeb;padding:20px">	
 		<p><label for="name">Name:</label><input name="name" id="name" type="text"></p>
 		<p><label for="redaktion">Redaktion:</label><input name="redaktion" id="redaktion" type="text"></p>
 		<p><label for="email">E-Mail:</label><input name="email" id="email" type="text"></p>
 		<p><label for="telefon">Telefon:</label><input name="telefon" id="telefon" type="text"></p>
 		<p><input type="hidden" name="send" value="true">
 		<input name="submit" id="submit" type="submit" value="Absenden" class="button"></p>
-
+		
 		<p><small><strong>Hinweis:</strong> Ihre Daten werden nur f&uuml;r die Bearbeitung der Registrierung verwendet.</small></p>
 	</fieldset>
 	</form>
