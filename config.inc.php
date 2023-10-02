3b101bc241d57e5a98987b580d4e6948b89d1929
pwafzig <pe.wafzig@gmail.com>
2023-10-02 17:32:12 +0200

    Delete / ignore files

--- config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	/************** Installationsverzeichnis **************/
-  define('INSTALLPATH', '');
-  define('DOCROOT', '');
-	define('HTTPHOST', '');
-	define('DEBUG'), 'false');
-
-?>
